809a8bd2dedfe182b527cecee7f232511daf6a92
Tasmia Hossain <tasmiahossain360@gmail.com>
2026-09-24 22:47:25 +0600

    Configure environment variables with NestJS Config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
@@ -7,9 +8,12 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
+  constructor(configService: ConfigService) {
+    const connectionString =
+      configService.getOrThrow<string>('DATABASE_URL');
+
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString,
     });
 
     super({ adapter });
